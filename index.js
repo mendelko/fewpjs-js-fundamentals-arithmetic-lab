0de7199f2f92b49a4ca7790d458bc40639eb809a
Mendel <mkonikov123@gmail.com>
2021-04-27 16:29:41 -0400

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const num1 = 2;
 const num2 = 31;
 const multiply = num1 * num2;
 
-const random = Number.parseInt(Math.random() * 10) + 1;
+const random = Number.parseInt(Math.random() * 10);
 
 const num3 = 4;
 const num4 = 10;
