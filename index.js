886b48f5bb5c9dd2061c178d9c0b4b66ad71152d
Mendel <mkonikov123@gmail.com>
2021-04-27 16:19:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Number.parseInt(Math.random() * 10) + 1;
+
+const num3 = 4;
+const num4 = 10;
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 15, 20);
+
